0075a8e01a154cc40879e274797b33df50b93612
pliniocamara <plinio.camara@outlook.com>
2022-11-20 11:31:50 -0300

    finalizando api com métodos PUT e DELETE

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const users = require('../mocks/users');
+let users = require('../mocks/users');
 
 module.exports = {
     listUsers(req, res) {
@@ -28,24 +28,51 @@ module.exports = {
     },
 
     createUser(req, res) {
-        let body = '';
+        const { body } = req;
 
-        req.on('data', (chunk) => {
-            body += chunk;
+        const lastUserId = users[users.length - 1].id;
+        const newUser = {
+            id: lastUserId + 1,
+            name: body.name,
+        };
+
+        users.push(newUser);
+
+        res.send(200, newUser);
+    },
+
+    updateUser(req, res) {
+        let { id } = req.params;
+        id = Number(id);
+
+        const { name } = req.body;
+
+        const userExists = users.find((user) => user.id === id);
+
+        if (!userExists) {
+            return res.send(400, { error: 'User not found'});
+        }
+
+        users = users.map((user) => {
+            if (user.id === id) {
+                return {
+                    ...user,
+                    name,
+                };
+            }
+
+            return user;
         });
 
-        req.on('end', () => {
-            body = JSON.parse(body);
-
-            const lastUserId = users[users.length - 1].id;
-            const newUser = {
-                id: lastUserId + 1,
-                name: body.name,
-            };
-
-            users.push(newUser);
-
-            res.send(200, newUser);
-        });    
+        res.send(200, { id, name });
     },
+
+    deleteUser(req, res) {
+        let { id } = req.params;
+        id = Number(id);
+
+        users = users.filter((user) => user.id !== id);
+
+        res.send(200, { deleted: true });
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 
+const bodyParser = require('./helpers/bodyParser');
 const routes = require('./routes');
 
 const server = http.createServer((req, res) => {
@@ -30,7 +31,11 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(body)); 
         };
 
-        route.handler(req, res);
+        if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+            bodyParser(req, () => route.handler(req, res));
+        } else {
+            route.handler(req, res);
+        }
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end(`Cannot ${req.method} ${pathname}`);
